95abd180998ff74498dfd699ec17724480ad410d
aliynajjar <ali.y.alnajjar@gmail.com>
2023-04-01 18:41:25 +0300

    Added mongoDB connection

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+require("dotenv").config();
+
+
+app.listen(process.env.PORT, (err) => {
+  if (err) console.error(err)
+  console.log("Server is listening on port", process.env.PORT)
+  require("./config/db.config")
+})
+
+app.use("/me/:name",(req,res)=>{
+  const name = req.params.name
+  res.json({message: `Hello World from ${name}`})
+})
+
+app.use("/",(req,res)=>{
+  res.json({message: "Hello World!"})
+})
+
+
