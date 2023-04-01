2b81874afeaf49a1df541c56dceabbfe5fc52769
aliynajjar <ali.y.alnajjar@gmail.com>
2023-04-01 21:51:56 +0300

    Added login and register apis

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,16 @@
 const express = require('express');
 const app = express();
-app.use(express.json());
 require("dotenv").config();
+app.use(express.json());
 
+const authRouter = require("./routes/auth.routes")
+
+app.use('/auth',authRouter);
 
 app.listen(process.env.PORT, (err) => {
-  if (err) console.error(err)
+  if (err) console.error('Here is the err')
   console.log("Server is listening on port", process.env.PORT)
   require("./configs/db.config")
-})
-
-app.use("/me/:name",(req,res)=>{
-  const name = req.params.name
-  res.json({message: `Hello World from ${name}`})
-})
-
-app.use("/",(req,res)=>{
-  res.json({message: "Hello World!"})
 })
 
 
