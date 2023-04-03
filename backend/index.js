642efcc19cb97775256bcfb72a8e1f831fa84558
aliynajjar <ali.y.alnajjar@gmail.com>
2023-04-03 03:41:58 +0300

    Added cors and changed login from get to post api

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 require("dotenv").config();
 app.use(express.json());
+const cors = require('cors')
+app.use(cors())
 
 const authRouter = require("./routes/auth.routes")
 const courseRouter = require("./routes/course.routes")
--- backend/routes/upload.routes.js
+++ backend/routes/upload.routes.js
@@ -2,6 +2,8 @@ const { Router } = require("express");
 const router = Router();
 
 const { createFile , getAllFiles } = require("../controllers/file.controller");
+const { authMiddleware } = require("../middlewares/auth.middleware");
+const { adminMiddleware } = require("../middlewares/admin.middleware");
 
 router.post("/upload", authMiddleware , adminMiddleware,createFile);
 router.get("/all", authMiddleware , adminMiddleware,getAllFiles);
